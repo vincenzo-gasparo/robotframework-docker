e020da68136d7452447f8752946b9320835293fd
Vincenzo Gasparo <vincenzo.gasparo@facile.it>
2020-10-15 19:38:33 +0200

    $ROBOTARGS

--- bin/run.sh
+++ bin/run.sh
@@ -4,9 +4,9 @@ cd /$TESTDIR
 if [ "$THREADS" -eq 0 ]; then
 echo "#========! EXECUTING COMMAND robot $ROBOTARGS --outputdir /outputs/ $ROBOT_FILES !========#"
 xvfb-run -a --server-args="-screen 0 $XVFB_RES -ac -nolisten tcp -dpi 96 +extension RANDR" \
-        robot --outputdir /outputs/ $ROBOT_FILES
+        robot $ROBOTARGS --outputdir /outputs/ $ROBOT_FILES
 else
 echo "#========! EXECUTING COMMAND pabot $ROBOTARGS --outputdir /outputs/ $ROBOT_FILES !========#"
 xvfb-run -a --server-args="-screen 0 $XVFB_RES -ac -nolisten tcp -dpi 96 +extension RANDR" \
-        pabot --outputdir /outputs/ $ROBOT_FILES
+        pabot $ROBOTARGS --outputdir /outputs/ $ROBOT_FILES
 fi
