00277beb645a8c117000305804ea68d2e59f3ef8
Vincenzo Gasparo <vincenzo.gasparo@facile.it>
2020-11-30 12:08:55 +0100

    SLACK_LISTENER fix

--- bin/run.sh
+++ bin/run.sh
@@ -4,8 +4,9 @@ set -e
 [ -z "$ROBOT_COMMAND" ] && ROBOT_COMMAND="robot"
 [ -z "$ROBOT_FILES" ] && ROBOT_FILES="."
 [ -z "$SLACKREPORTS" ] && SLACKREPORTS="false"
-[ "$SLACKREPORTS" == "true" ] && SLACK_LISTENER="--listener /tmp/SlackNotifier.py:${SLACK_HOOK}"
-
+if [ "$SLACKREPORTS" = "true" ]; then
+    SLACK_LISTENER="--listener /tmp/SlackNotifier.py:${SLACK_HOOK}"
+fi
 echo "#========! EXECUTING COMMAND $ROBOT_COMMAND $ROBOT_ARGS --outputdir /output/ $ROBOT_FILES !========#"
 xvfb-run -a --server-args="-screen 0 $XVFB_RES -ac -nolisten tcp -dpi 96 +extension RANDR" \
 $ROBOT_COMMAND $ROBOT_ARGS $ROBOT_ARGS_MASK $SLACK_LISTENER --outputdir /output/ $ROBOT_FILES
